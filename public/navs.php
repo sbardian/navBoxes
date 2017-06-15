06616f9eb65d8bc3198b78c9c5f2c1682965d15c
Brian <sbardian@gmail.com>
2017-06-14 21:50:24 -0600

    Add fake cards for testing.

--- public/navs.php
+++ public/navs.php
@@ -8,4 +8,85 @@
 
 ?>
 
-<h1>NavBoxes</h1>
+<div class="container">
+  <div class="nav-cards">
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+    <a class="nav-card" href="#">
+			<span class="nav-card-header" style="background-image: url(http://192.168.1.126/wp-content/plugins/navBoxes/public/images/animals.jpeg);">
+				<span class="nav-card-title">
+					<h3>This is a title for a card</h3>
+				</span>
+			</span>
+      <span class="nav-card-summary">
+				A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+			</span>
+      <span class="nav-card-meta">
+				Published: June 18th, 2015
+			</span>
+    </a>
+  </div>
+</div>
